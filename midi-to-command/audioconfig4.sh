24947ac89d2294048b706b6f2710ece4530b02b8
RowdyVoyeur <102534097+RowdyVoyeur@users.noreply.github.com>
2023-05-17 12:06:59 +0100

    Create audioconfig4.sh

---
+++ midi-to-command/audioconfig4.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Get list of connected ports
+connected_ports=$(jack_lsp -c | grep -v '^ ')
+
+if [[ -z "$connected_ports" ]]; then
+    echo "No connected ports found"
+else
+    echo "Connected ports:"
+    echo "$connected_ports"
+    # Loop through each connected port
+    while read -r port; do
+        # Get list of connections for this port
+        connections=$(jack_lsp -c "$port" | grep '^ ')
+
+        if [[ -z "$connections" ]]; then
+            echo "No connections found for: $port"
+        else
+            # Disconnect each connection for this port
+            while read -r connection; do
+                echo "Disconnecting: $port -> ${connection#*> }"
+                jack_disconnect "$port" "${connection#*> }"
+            done <<< "$connections"
+        fi
+    done <<< "$connected_ports"
+fi
+
+sleep 1
+
+# Connect the required ports
+jack_connect system:capture_1 M8_out:playback_1
+jack_connect system:capture_1 M8_out:playback_2
+jack_connect M8_in:capture_1 MC101_out:playback_1
+jack_connect M8_in:capture_2 MC101_out:playback_2
+jack_connect MC101_in:capture_1 system:playback_1
+jack_connect MC101_in:capture_2 system:playback_2
