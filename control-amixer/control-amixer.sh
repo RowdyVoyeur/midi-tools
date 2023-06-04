b6a9c058ac50203b5edfd90f4a77cdbae181d807
RowdyVoyeur <102534097+RowdyVoyeur@users.noreply.github.com>
2023-06-05 00:12:48 +0100

    Create control-amixer.sh

---
+++ control-amixer/control-amixer.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Start listening to MIDI messages
+echo "Script started. Listening to MIDI messages from nanoKONTROL..."
+
+# Capture MIDI data sent by nanoKONTROL on CC 118 and control Main Output Volume
+aseqdump -p "nanoKONTROL" | \
+sed -nur 's/^.*controller 118, value (.*)/\1/p' | \
+while read l; do
+    amixer -D hw:Device sset Speaker "$l"%
+    echo "Received MIDI message - CC118: $l"
+done &
+
+# Capture MIDI data sent by nanoKONTROL on CC 119 and control Main Input Volume
+aseqdump -p "nanoKONTROL" | \
+sed -nur 's/^.*controller 119, value (.*)/\1/p' | \
+while read l; do
+    amixer -D hw:Device sset Mic "$l"%
+    echo "Received MIDI message - CC119: $l"
+done
