c6cb5610cd2d31ab97f93ac9c2fc6ffe52ba4551
RowdyVoyeur <102534097+RowdyVoyeur@users.noreply.github.com>
2023-07-07 10:13:53 +0100

    Create audioconfig7.sh

---
+++ midi-to-command/audioconfig7.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Get list of connected ports
+connected_ports=$(jack_lsp -c | grep -v '^ ')
+
+if [[ -z "$connected_ports" ]]; then
+    echo "No connected ports found"
+else
+    echo "Connected ports:"
+    echo "$connected_ports"
+    # Loop through each connected port
+    while read -r port; do
+        # Get list of connections for this port
+        connections=$(jack_lsp -c "$port" | grep '^ ')
+
+        if [[ -z "$connections" ]]; then
+            echo "No connections found for: $port"
+        else
+            # Disconnect each connection for this port
+            while read -r connection; do
+                echo "Disconnecting: $port -> ${connection#*> }"
+                jack_disconnect "$port" "${connection#*> }"
+            done <<< "$connections"
+        fi
+    done <<< "$connected_ports"
+fi
+
+sleep 1
+
+# Connect audio of M8 Out to MC101 In
+jack_connect M8_in:capture_1 MC101_out:playback_1
+jack_connect M8_in:capture_2 MC101_out:playback_2
+
+# Connect audio of USB Card Microphone to MC101 In
+jack_connect system:capture_1 MC101_out:playback_1
+jack_connect system:capture_1 MC101_out:playback_2
