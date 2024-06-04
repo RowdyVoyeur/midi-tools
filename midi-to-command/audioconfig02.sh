62103d110893d02108316150b90ae847dd870159
RowdyVoyeur <102534097+RowdyVoyeur@users.noreply.github.com>
2024-06-04 09:47:23 +0100

    Update audioconfig02.sh

--- midi-to-command/audioconfig02.sh
+++ midi-to-command/audioconfig02.sh
@@ -25,17 +25,29 @@ else
     done <<< "$connected_ports"
 fi
 
+# Audio Routing: IN->MC101->M8->OUT
+
 sleep 1
 
+# Check if the Instrument with the Card Name "MC101" is connected to the Raspberry Pi
+# Use "aplay -l" to find the Card Name of any connected audio devices
+if [ $(aplay -l | grep -c "MC101") -eq 0 ]; then
+  echo "MC101 not detected, skipping connection."
+else
+  echo "MC101 detected, connecting."
+
 # Connect audio of USB Card Microphone or Audio Card In to MC101 In
-jack_connect system:capture_1 MC101_out:playback_1
-# Comment the following line and uncomment the next one, if your USB Card or Audio Card In has a mono ADC
-jack_connect system:capture_2 MC101_out:playback_2
-# jack_connect system:capture_1 MC101_out:playback_2
+# If USB Card or Audio Card In has a Mono ADC, use system:capture_1 in both lines, which creates a fake stereo
+# If MC101 is in Vendor Driver Mode, use MC101_out:playback_3 and MC101_out:playback_4
+# If MC101 is in Generic Driver Mode, use MC101_out:playback_1 and MC101_out:playback_2
+jack_connect system:capture_1 MC101_out:playback_3
+jack_connect system:capture_2 MC101_out:playback_4
 
 # Connect audio of MC101 Out to M8 In
 jack_connect MC101_in:capture_1 M8_out:playback_1
 jack_connect MC101_in:capture_2 M8_out:playback_2
+
+fi
 
 # Connect audio of M8 Out to System In
 jack_connect M8_in:capture_1 system:playback_1
